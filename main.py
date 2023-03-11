47a2835504b1056e67ac4b8041bcbe9c0ef368f9
1Dorian <127608744+1Dorian@users.noreply.github.com>
2023-03-11 15:33:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pygame
-   
-def main():
-    pygame.init()
-    pygame.display.set_caption("Anti-virus")
-    screen=pygame.display.set_mode((0,0),pygame.FULLSCREEN)
-    background,écart_x,écart_y=fond_d_écran(screen,"uni")
-    taille=(min(screen.get_size()))//18
 
+def menu():
+   return
+
+def selection_difficulté():
+   return
+
+
+def main():
+   pygame.init()
+   pygame.display.set_caption("Anti-virus")
+   screen=pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+   background,écart_x,écart_y=fond_d_écran(screen,"uni")
+   taille=(min(screen.get_size()))//18
+   menu()
+   
 main()
