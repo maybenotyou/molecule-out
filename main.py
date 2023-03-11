78bfa4589d7f684c87b624ef9e866712f49b2931
1Dorian <127608744+1Dorian@users.noreply.github.com>
2023-03-11 17:10:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ def main():
    pygame.init()
    pygame.display.set_caption("Anti-virus")
    screen=pygame.display.set_mode((0,0),pygame.FULLSCREEN)
-   background,écart_x,écart_y=fond_d_écran(screen,"uni")
    taille=(min(screen.get_size()))//18
    menu()
    
