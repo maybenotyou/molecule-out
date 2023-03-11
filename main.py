52cccd57887eef844374f9e9905bc8fbde776cee
1Dorian <127608744+1Dorian@users.noreply.github.com>
2023-03-11 15:36:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame
 
+def starter():
+   return
+
+def junior():
+   return
+
+def expert():
+   return
+
+def maitre():
+   return
+
+def difficulte():
+   return
+
 def menu():
    return
-
-def selection_difficulte():
-   return
-
 
 def main():
    pygame.init()
