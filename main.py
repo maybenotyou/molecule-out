fe5ff5081a69bead178597cedac1fab35b51a8ff
1Dorian <127608744+1Dorian@users.noreply.github.com>
2023-03-12 16:39:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ def rectangle_inscrit(rayon):
     return int((2*(rayon**2))**0.5)
 
 class Bouton_circulaire():
-    def __init__(self,x,y,rayon,couleur,surface):
+    def __init__(self,x,y,rayon,couleur,surface,nom):
         self.x=x
         self.y=y
         self.couleur=couleur
         self.rayon=rayon
         self.surface=surface
+        self.nom=nom
         self.selectionné=False
         self.longueur=rectangle_inscrit(rayon)
         self.rect = pygame.Rect((self.x-(self.longueur//2),self.y-(self.longueur//2)),(self.longueur,self.longueur))
         self.surface_bouton=pygame.Surface((self.rect[2],self.rect[3]))
 
     def update(self):
-            pygame.draw.circle(self.surface,self.couleur,(self.x,self.y), self.rayon, 0)
-
+            pygame.draw.circle(self.surface,self.couleur,(self.x,self.y), self.rayon)
+            
 class Bouton_rectangulaire():
     def __init__(self,x,y,hauteur,largeur,couleur,surface,nom,taille):
         self.x=x
@@ -59,16 +60,20 @@ def difficulte(surface,taille):
     background = pygame.Surface(surface.get_size())
     background.fill((200,200,200))
     background.convert()
+
     titre=pygame.font.SysFont(None,4*taille).render('Choisissez la difficulté',True,(0, 0, 0))
+
     Starter=Bouton_rectangulaire(int(surface.get_size()[0]/6-5*taille),int(4*surface.get_size()[1]/10),3*taille,10*taille,(250,250,250),surface,'Starter',3*taille)
     Junior=Bouton_rectangulaire(int(surface.get_size()[0]/6-5*taille),int(7*surface.get_size()[1]/10),3*taille,10*taille,(250,250,250),surface,'Junior',3*taille)
     Expert=Bouton_rectangulaire(int(3*surface.get_size()[0]/6-5*taille),int(4*surface.get_size()[1]/10),3*taille,10*taille,(250,250,250),surface,'Expert',3*taille)
     Master=Bouton_rectangulaire(int(3*surface.get_size()[0]/6-5*taille),int(7*surface.get_size()[1]/10),3*taille,10*taille,(250,250,250),surface,'Master',3*taille)
     Wizard=Bouton_rectangulaire(int(5*surface.get_size()[0]/6-5*taille),int(4*surface.get_size()[1]/10),3*taille,10*taille,(250,250,250),surface,'Wizard',3*taille)
     Bonus=Bouton_rectangulaire(int(5*surface.get_size()[0]/6-5*taille),int(7*surface.get_size()[1]/10),3*taille,10*taille,(250,250,250),surface,'Bonus',3*taille)
-    liste_boutons=[[Starter,Expert,Wizard],[Junior,Master,Bonus]]
-    i=0
-    j=0
+    Menu=Bouton_circulaire(int(1.5*taille),int(1.5*taille),taille,(250,250,250),surface,'Menu')
+
+    liste_boutons=[[Menu],[Starter,Expert,Wizard],[Junior,Master,Bonus]]
+    i=1
+    j=1
     b=liste_boutons[i][j]
     running=True
     while running:
@@ -77,6 +82,7 @@ def difficulte(surface,taille):
                 running=False
                 pygame.quit()
                 return
+
             elif event.type==pygame.KEYDOWN:
                 if event.key==pygame.K_ESCAPE:
                     running=False
@@ -98,21 +104,26 @@ def difficulte(surface,taille):
                     b=liste_boutons[i][j]
 
                 elif event.key==pygame.K_UP:
-                    if i<len(liste_boutons)-1:
-                        i+=1
-                    else:
-                        i=0
-                    b=liste_boutons[i][j]
-
-                elif event.key==pygame.K_DOWN:
                     if i>0:
+                        if i==1:
+                            j=0
                         i-=1
                     else:
                         i=len(liste_boutons)-1
                     b=liste_boutons[i][j]
 
+                elif event.key==pygame.K_DOWN:
+                    if i<len(liste_boutons)-1:
+                        i+=1
+                    else:
+                        i=0
+                        j=0
+                    b=liste_boutons[i][j]
+
                 elif event.key==pygame.K_SPACE or event.key==pygame.K_RETURN:
-                    if b.nom=='Starter':
+                    if b.nom=='Menu':
+                        return menu()
+                    elif b.nom=='Starter':
                         return starter()
                     elif b.nom=='Junior':
                         return junior()
@@ -125,12 +136,15 @@ def difficulte(surface,taille):
                     elif b.nom=='Bonus':
                         return bonus()
 
-        rect_sel=pygame.Rect((b.rect[0]-int(taille/10),b.rect[1]-int(taille/10)),(b.rect[2]+2*int(taille/10),b.rect[3]+2*int(taille/10)))
-
         surface.blit(background,(0,0))
-        surface.blit(titre,(int((surface.get_size()[0]-titre.get_size()[0])/2),int(surface.get_size()[1]/10)))
+        surface.blit(titre,(int((surface.get_size()[0]-titre.get_size()[0])/2),int(4*surface.get_size()[1]/25)))
 
-        pygame.draw.rect(surface,(100,100,100),rect_sel)
+        if i==0:
+            pygame.draw.circle(surface,(100,100,100),(b.x,b.y), int(1.1*b.rayon))
+
+        else:
+            rect_sel=pygame.Rect((b.rect[0]-int(taille/10),b.rect[1]-int(taille/10)),(b.rect[2]+2*int(taille/10),b.rect[3]+2*int(taille/10)))
+            pygame.draw.rect(surface,(100,100,100),rect_sel)
 
         Starter.update()
         Junior.update()
@@ -138,6 +152,7 @@ def difficulte(surface,taille):
         Master.update()
         Wizard.update()
         Bonus.update()
+        Menu.update()
 
         pygame.display.update()
 
